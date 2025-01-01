5fc30d3d1314dc677c4b284d16ad2288284e1dd1
sahar <saharghazanfariast@gmail.com>
2025-01-02 02:20:27 +0330

    chart and lang

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -20,27 +20,3 @@ export const LanguageProvider = ({ children }) => {
 };
 
 export const useLanguage = () => useContext(LanguageContext);
-
-// // src/context/LanguageContext.js
-// "use client";
-// import React, { createContext, useContext, useState } from "react";
-
-// const LanguageContext = createContext();
-
-// export function LanguageProvider({ children }) {
-//   const [language, setLanguage] = useState("en");
-
-//   return (
-//     <LanguageContext.Provider value={{ language, setLanguage }}>
-//       {children}
-//     </LanguageContext.Provider>
-//   );
-// }
-
-// export function useLanguage() {
-//   const context = useContext(LanguageContext);
-//   if (!context) {
-//     throw new Error("useLanguage must be used within a LanguageProvider");
-//   }
-//   return context;
-// }
